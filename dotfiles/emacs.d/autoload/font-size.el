ec9be7f9673b8aa78f66f565bba045ac2913e72f
Alagner <mmiera@poczta.fm>
2019-12-25 00:16:58 +0100

    add default value to interactive font size call

--- dotfiles/emacs.d/autoload/font-size.el
+++ dotfiles/emacs.d/autoload/font-size.el
@@ -3,7 +3,9 @@
 (defun font-size-set ( &optional  points )
 "Convenience function for setting font size to POINTS.
 Default value taken from variable font-size-default."
-(interactive "nSize:")
+(interactive
+  (list (string-to-number (read-string "Size: " (number-to-string font-size-default))))
+)
   (unless points (setq points font-size-default))
   (set-face-attribute 'default nil :height (* 10 points))
 )
@@ -19,11 +21,12 @@ Default value taken from variable font-size-default."
 
 
 (defun font-size-decrement ()
-  "Increase font size by one point"
-  (interactive nil)
+"Increase font size by one point"
+(interactive nil)
   (let ((current-size (/ (face-attribute 'default :height) 10)  ))
     (font-size-set (- current-size 1))
   )
 )
 
 (provide 'font-size)
+
